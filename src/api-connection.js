bc6b67bbfa1d2c5e507094f5021ea980a48ea817
Becka <rebecca.lelew@gmail.com>
2018-11-17 13:55:41 +0000

    Fixes Api connection

--- src/api-connection.js
+++ src/api-connection.js
@@ -1,6 +1,21 @@
+
 ApiConnection= function() {
   this.apiKey = apiKey
 }
 
-ApiConnection.prototype.methodName = function () {
+ApiConnection.prototype.getNews = function () {
+  api = 'https://content.guardianapis.com/search?api-key='
+  fullAddress = api + this.apiKey
+
+  fetch(
+    fullAddress, {
+    // mode: 'cors'
+  })
+  .then(function(response) {
+      return response.json()
+    })
+    .then(function(t) {
+      console.log(t);
+    });
+
 };
